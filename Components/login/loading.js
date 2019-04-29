a1b28f7c4c5614ee5c2a95ae9ffe8281e3e79685
Hiran Tharinda <hirantharinda@gmail.com>
2019-04-29 14:39:45 +0530

    loadingStyles

--- Components/login/loading.js
+++ Components/login/loading.js
@@ -1,27 +1,44 @@
 import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet, Image, StatusBar } from 'react-native'
 import firebase from 'react-native-firebase'
 
 export default class Loading extends React.Component {
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
-        this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+            this.props.navigation.navigate(user ? 'Main' : 'SignUp')
         })
     }   
 
     render() {
         return (
+        
         <View style={styles.container}>
-            <Text>Loading</Text>
-            <ActivityIndicator size="large" />
+        <StatusBar
+            barStyle = "dark-content"
+            hidden = {false}
+            backgroundColor = "#48dbfb"
+            translucent = {true}
+            networkActivityIndicatorVisible = {false}
+        />
+            <View style={{flex:3}}></View> 
+            <View style={{flex:2}}></View> 
+            <View style={{flex:1}} >
+                <Image source = {require('../../assets/splashCozy.png')} style = {{resizeMode: 'contain'}}/>
+            </View>
+            <View style={{flex:4}}></View> 
+            <View style={{flex:1}}>
+                <ActivityIndicator size="large" color="#ffffff" />
+            </View>
         </View>
         )
     }
 }
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#48dbfb'
   }
 })
